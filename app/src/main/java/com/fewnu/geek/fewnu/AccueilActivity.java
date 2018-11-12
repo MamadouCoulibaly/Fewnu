9ee03d406fad8569a517ba145b0415e4e8abef72
dioula97 <balboucka@gmail.com>
2018-11-12 10:59:58 +0000

    Integration StringBuilder pour le numéro de phone

--- app/src/main/java/com/fewnu/geek/fewnu/AccueilActivity.java
+++ app/src/main/java/com/fewnu/geek/fewnu/AccueilActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
